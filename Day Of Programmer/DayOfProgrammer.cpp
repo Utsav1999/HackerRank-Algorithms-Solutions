6f1b54fe000681023f1b6581c659c063f2ccb5c5
pratik <pratikprogrammer229@gmail.com>
2021-10-01 14:57:44 +0530

    Day Of Programmer solved from hackerrank updated with comments

--- Day Of Programmer/DayOfProgrammer.cpp
+++ Day Of Programmer/DayOfProgrammer.cpp
@@ -9,9 +9,24 @@ using namespace std;
 
 void day(int year)
 {
+    /*
+    INTUITION:
+    Overall basic intuition is if year is leap in any calendar julian or gregorian
+    day of programmer will fall on 12 sept and if not it will be on 13 sept
+    
+    */
+
     int date;
+    //check if the year lies before 1918 and greater than equal to1700
+    //this means its in julian calendar
+
+
+
     if(year>=1700 && year<=1917)
     {
+        // if year lies then check wheteher it us leap
+        //in julian calendar leap year is divisible by 4
+
         if(year % 4==0)
         {
             date=12;
@@ -22,6 +37,13 @@ void day(int year)
 
         }
     }
+    /*
+    February  contained 28  (the usual number of days in a non-leap year) 
+    minus 13 (the number of missed days as the  was the first day of the month) equal to  days.
+    so in 1918 there were total 215 excluding 13 days in feb,  so 256-215=41 and then 15 days were missed
+    as 14 was first day of february so 41-15 = 26
+    hence date is 26 in 1918
+    */
     else if (year==1918)
     {
         date=26;
